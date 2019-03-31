9e2fba8e616561318517f4b7f42dccafe4fe782c
jodnddus <lockhost23@gmail.com>
2019-03-31 22:40:26 +0900

    서버 코드 copy&paste

---
+++ database/db_config.js
@@ -0,0 +1,9 @@
+module.exports = (() => {
+    return {
+        host    : 'localhost',
+        user    : 'root',
+        password: '111111',
+        database: 'video_seller',
+        port    : '3306'
+    }
+})();
