8f2b4907bab148e094412b51b1d7a7b3fcf557a2
jodnddus <lockhost23@gmail.com>
2019-04-01 21:21:52 +0900

    데이터 분기

---
+++ graphql/data/user.ts
@@ -0,0 +1,25 @@
+let Users = [
+    {
+        id: 0,
+        username: "jodnddus",
+        email: "lockhost23@gmail.com",
+        password: "asdf1145",
+        videoid: [100, 102]
+    },
+    {
+        id: 1,
+        username: "leechanggeun",
+        email: "lee2019@gmail.com",
+        password: "abcd1234",
+        videoid: [100, 101, 102, 103, 104]
+    },
+    {
+        id: 2,
+        username: "gangyeonjo",
+        email: "gangyeonjo336@gmail.com",
+        password: "bbaaccdd",
+        videoid: [100, 104]
+    }
+];
+
+export default Users;
---
+++ graphql/data/video.ts
@@ -0,0 +1,29 @@
+let Videos = [
+    {
+        id: 100,
+        name: "부산행",
+        price: 6000
+    },
+    {
+        id: 101,
+        name: "어벤져스: 엔드게임",
+        price: 10000
+    },
+    {
+        id: 102,
+        name: "극한직업",
+        price: 9000
+    },
+    {
+        id: 103,
+        name: "더 테러 라이브",
+        price: 5000
+    },
+    {
+        id: 104,
+        name: "곡성",
+        price: 7000
+    }
+]
+
+export default Videos;
